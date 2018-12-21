5f1a386525b320ea8ae723e452ef6cee3da9cde2
thurbridi <arthurbridi2005@gmail.com>
2018-12-21 17:39:09 -0200

    Removed unused class

--- app/src/main/java/thurbridi/inventorymanager/ItemDetailViewModel.kt
+++
@@ -1,8 +0,0 @@
-package thurbridi.inventorymanager
-
-import android.app.Application
-import android.arch.lifecycle.AndroidViewModel
-
-class ItemDetailViewModel(application: Application): AndroidViewModel(application) {
-
-}
